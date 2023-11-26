7bbdbdd7885208836566273cfdaafdc3ca062418
CHT2110 <cahathomassen@gmail.com>
2023-11-26 12:08:47 +0100

    creating and schema and tables

--- mysql_connector.py
+++ mysql_connector.py
@@ -15,34 +15,103 @@ def connector():
         database = config['database']
     )
 
-    cursor = mysql_connection.cursor()
+    return mysql_connection
 
-    return cursor
-
-def create__drop_schema(connection, cursor):
+def create_drop_schema(connection, cursor):
     # Dropping existing schema to delete outdated data.
     # When patching happens we aren't interested in outdated data, and this saves storage.
-    cursor.execute('DROP SCHEMA IF EXISTS source')
+    cursor.execute('DROP SCHEMA sources')
 
     # Re-creating the schema
     cursor.execute('CREATE SCHEMA sources')
 
-    connection.commit()
-
-def create_table(connection, cursor, data):
-    # Creating dynamic columns in case riot decides to chance column namesS
-    column_definitions = ', '.join([f'{key} VARCHAR(255) NOT NULL' for key in data[0].keys()])
-    create_table_query = f"""
-        CREATE TABLE IF NOT EXISTS source.summoners (
-            {column_definitions}
-        )
-    """
-
-    #Inserting data into the tables
-    for summoner in data:
-        column_names = ', '.join(summoner.keys())
-        column_values = ', '.join([f"'{value}'" for value in summoner.values()])
-        insert_query = f"INSERT INTO source.summoners ({column_names}) VALUES ({column_values})"
-        cursor.execute(insert_query)
+    cursor.execute('USE sources')
 
     connection.commit()
+
+def create_tables(connection, cursor):
+    # Creating tables for data
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS summoners (
+        summoner_id varchar(200) primary key,
+        summoner_name varchar(200),
+        league_points int,
+        tier varchar(50),
+        wins int,
+        losses int,
+        veteran boolean,
+        inactive boolean,
+        fresh_blood boolean,
+        hot_streak boolean
+    );''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS player_info (
+        summoner_id varchar(200) primary key,
+        account_id varchar(200),
+        puu_id varchar(200),
+        summoner_name varchar(200),
+        profile_icon_id int,
+        revision_date int,
+        summoner_level int
+    );''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS match_data (
+        match_id VARCHAR(20) primary key,
+        data_version VARCHAR(10),
+        game_datetime BIGINT,
+        game_length FLOAT,
+        game_version VARCHAR(50)
+    );''')    
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS participants (
+        puuid varchar(64) primary key,
+        match_id varchar(20),
+        gold_left int,
+        last_round int,
+        level int,
+        placement int,
+        players_eliminated int,
+        time_eliminated float,
+        total_damage_to_players int
+    );''')
+    
+    cursor.execute('''CREATE TABLE IF NOT EXISTS traits (
+        id int auto_increment primary key,
+        puuid varchar(64),
+        name varchar(50),
+        num_units int,
+        style int,
+        tier_current int,
+        tier_total int
+    );''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS units (
+        id int auto_increment primary key,
+        puuid varchar(64),
+        character_id varchar(20),
+        itemnames text,
+        name varchar(50),
+        rarity int,
+        tier int
+    );''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS companions (
+        id int auto_increment primary key,
+        puuid varchar(64),
+        content_id varchar(36),
+        item_id int,
+        skin_id int,
+        species varchar(50)
+    );''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS augments (
+        id int auto_increment primary key,
+        puuid varchar(64),
+        augment_name varchar(50)
+    );''')
+
+    connection.commit()
+
+    cursor.close()
+
+    connection.close()
